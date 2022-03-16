c8180bfe6475545016ea3413d93a5ca7c12f990d
Hellen Wainaina <58620060+hellen-22@users.noreply.github.com>
2022-03-16 09:13:47 +0300

    creating API's in node

---
+++ MVC REST API/routes/api/employees.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const employeeController = require('./../../controllers/employeesController')
+
+router.route('/')
+    .get(employeeController.getAllEmployees)
+    .post(employeeController.createNewEmployee)
+    .put(employeeController.updateEmployee)
+    .delete(employeeController.deleteEmployee);
+
+
+router.route('/:id')
+    .get(employeeController.getAnEmployee);
+
+
+module.exports = router;
